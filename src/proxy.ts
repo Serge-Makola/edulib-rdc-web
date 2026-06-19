e5eb6d0fc26fd5aa557592791656edd4ff6a5883
Serge-Makola <sergemakola@outlook.fr>
2026-06-19 01:35:57 +0000

    fix: proxy.ts avec export middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -29,3 +29,5 @@ export function proxy(req: NextRequest) {
 export const config = {
   matcher: ['/dashboard/:path*', '/espace-direction/:path*', '/login', '/register'],
 }
+
+export { proxy as middleware }
